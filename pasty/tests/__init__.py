11926ec1d87c94830385e336a425a745509e7ed5
David Buxton <david@gasmark6.com>
2016-09-05 09:19:55 +0100

    Setup testbed stubs individually, so tests can run without PIL.

--- pasty/tests/__init__.py
+++ pasty/tests/__init__.py
@@ -9,7 +9,13 @@ class AppEngineTestCase(TestCase):
 
         self.testbed = testbed.Testbed()
         self.testbed.activate()
-        self.testbed.init_all_stubs()
+        self.testbed.init_app_identity_stub()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        self.testbed.init_taskqueue_stub()
+        self.testbed.init_urlfetch_stub()
+        self.testbed.init_user_stub()
+        self.testbed.init_search_stub()
 
     def tearDown(self):
         self.testbed.deactivate()
