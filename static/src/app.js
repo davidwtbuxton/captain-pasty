3099fef6b5c524792ac9b3a3681db8f2d4e5619d
David Buxton <david@gasmark6.com>
2016-10-29 20:02:28 +0100

    Initial support for drag and drop of files.

--- static/src/app.js
+++ static/src/app.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
 	'use strict';
 
-	function star_paste() {
+	function starPaste() {
 		var paste_id = this.dataset.pasteId,
 			url = this.dataset.url,
 			data = {'paste': paste_id};
 
-		$.post(url, data, star_success);
+		$.post(url, data, starSuccess);
 	}
 
-	function star_success() {
+	function starSuccess() {
 		$('.star__action').addClass('is-disabled');
 		$('.star__action .star__status').html('Starred');
 		$('.star__action .fa').addClass('fa-star').removeClass('fa-star-o');
 	}
 
-	$('.star__action').click(star_paste);
+	$('.star__action').click(starPaste);
 
-	function add_file_inputs() {
+	function addFileInputs() {
 		/* Copy the filename and textarea inputs for a new file. */
 		var $old_group = $('.paste-form__file-group').last(),
 			$new_group = $old_group.clone();
@@ -25,11 +25,46 @@ $(document).ready(function() {
 		$new_group.find('.paste-form__filename').val('');
 		$new_group.find('.paste-form__content').val('');
 		$new_group.insertAfter($old_group);
+
+		return $new_group;
 	}
 
-	$('.paste-form__add-file').click(add_file_inputs);
+	$('.paste-form__add-file').click(addFileInputs);
 
 	$('.notification').on('click', '.delete', function() {
 		$(this).parent('.notification').remove();
+	});
+
+
+	function fileUploadSupported() {
+		/* True if we want to support drag-n-drop file uploads. */
+		return true;
+	}
+
+	$('.paste-form__file-group').on('dragenter dragover', function() {
+		$('input, textarea', this).addClass('drag-start');
+
+		return false;
+	});
+
+	$('.paste-form__file-group').on('dragleave dragend dragexit drop', function() {
+		$('input, textarea', this).removeClass('drag-start');
+
+		return false;
+	});
+
+	$('.paste-form__file-group').on('drop', function(evt) {
+		var files = evt.originalEvent.dataTransfer.files,
+			dropTarget = this;
+
+		$.each(files, function(idx, obj) {
+			$('.paste-form__filename', dropTarget).val(obj.name);
+
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				$('.paste-form__content', dropTarget).val(e.target.result);
+			};
+			reader.readAsText(obj);
+		});
 	});
 });
